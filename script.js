236118a5e69a8469afa323152b4d8646ea7325b1
Precious <Preshomondi@gmail.com>
2024-08-06 00:02:19 +0300

    Draw the Sierpinski triangle

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ ctx.closePath()
 ctx.fillStyle = color
 ctx.fill()
 }
+
+function sierpinski(p1, p2, p3, depth){
+    if(depth === 0){
+        drawTriangle(p1, p2, p3, 'white')
+    }else {
+        const p4 = {
+            x: (p1.x + p2.x) / 2,
+            y: (p1.y + p2.y) / 2
+        }
+        const p5 = {
+            x: (p2.x + p3.x) /2,
+            y: (p2.y + p3.y) / 2
+        }
+        const p6 = {
+            x: (p1.x + p3.x) / 2,
+            y: (p1.y + p2.y) / 2
+        }
+
+        sierpinski(p1, p4, p6, depth - 1)
+        sierpinski(p4, p2, p5, depth - 1)
+        sierpinski(p6, p5, p3, depth - 1)
+
+    }
+}
+
